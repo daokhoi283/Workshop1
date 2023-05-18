6e4c1c1641768067fc2481c2040796bc7d7e0165
daokhoi283 <khoind.283@gmail.com>
2023-05-19 01:11:04 +0700

    Workshop1

---
+++ Part2.java
@@ -0,0 +1,28 @@
+package workshop1;
+import java.util.Scanner;
+public class Part2 {
+    public static void main(String[] args) {
+        float num1, num2;
+        String op;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the number 1: ");
+        num1 = sc.nextFloat();
+        System.out.println("Input the number 2: ");
+        num2 = sc.nextFloat();
+        sc = new Scanner(System.in);
+        System.out.println("Input the operator(+-*/): ");
+        op = sc.nextLine();
+        if(op.equals("+")){
+            System.out.println("The result of " + num1 + op + num2 + "=" + (num1+num2));
+        }
+        else if(op.equals("-")){
+            System.out.println("The result of " + num1 + op + num2 + "=" + (num1-num2));
+        }
+        else if(op.equals("*")){
+            System.out.println("The result of " + num1 + op + num2 + "=" + (num1*num2));
+        }
+        else{
+            System.out.println("The result of " + num1 + op + num2 + "=" + (num1/num2));
+        }
+    }
+}
